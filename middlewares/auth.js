7092b542c158a946914fb551c8564b5ccdaa4af5
Sam Branham <hotdogsb@gmail.com>
2025-01-18 04:34:03 -0600

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/config");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  // error catch missing authorization
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.status(401).send({ message: "Authorization required." });
+  }
+
+  // get the payload
+  const token = authorization.replace("Bearer ", "");
+  let payload;
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(401).send({ message: "Authorization required" });
+  }
+
+  // pass the payload to further requests
+  req.user = payload;
+  next();
+};
