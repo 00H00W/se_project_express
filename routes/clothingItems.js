000fbfa7f1552353eac8264418bf2f0fad6b3456
Sam Branham <hotdogsb@gmail.com>
2025-01-18 04:37:56 -0600

    route auth middleware

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const itemController = require("../controllers/clothingItems");
+const auth = require("../middlewares/auth");
 
 router.get("/", itemController.getItems);
+router.use(auth);
 router.post("/", itemController.createItem);
 router.delete("/:itemId", itemController.deleteItem);
 router.put("/:itemId/likes", itemController.likeItem);
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userController = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
+router.use(auth);
 router.get("/:userId", userController.getUser);
 
 module.exports = router;
