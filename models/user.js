9fad4648d73204986cea4e0f24765fb6abee948b
Sam Branham <hotdogsb@gmail.com>
2025-01-18 04:12:07 -0600

    add findUserByCredentials method to user schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -34,5 +35,22 @@ const userSchema = mongoose.Schema({
     required: [true, "The password field is required."],
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect email or password"));
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return user;
+    });
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
